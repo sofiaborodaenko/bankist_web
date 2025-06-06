5d04e5d8b2a6e76071afd5d40cd9ddf5fc578506
Sofia Borodaenko <soph.bor06@gmail.com>
2025-06-06 15:41:05 -0400

    added a smooth scroll on the header links

--- starter/script.js
+++ starter/script.js
@@ -7,6 +7,9 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+
+const section1 = document.querySelector("#section--1");
 
 const header = document.querySelector(".header");
 
@@ -38,13 +41,32 @@ message.classList.add("cookie-message");
 message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 message.style.position = "sticky";
-message.style.top = "0px";
-message.style.bottom = "0px";
+message.style.top = "calc(100vh - 79px)";
 message.style.zIndex = "100";
 header.after(message);
+message.style.height =
+  Number.parseInt(getComputedStyle(message).height, 10) + 30 + "px";
 
 document
   .querySelector(".btn--close-cookie")
   .addEventListener("click", function () {
     message.remove();
   });
+
+// learn more button
+btnScrollTo.addEventListener("click", function () {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+// navigation links (using event delegation)
+// 1. add event listener to common parent element
+// 2. determine waht element originate the event
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
